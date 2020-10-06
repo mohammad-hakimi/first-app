84e6e2ef58d8ae5ed00139e79bf0c7590171c569
Mohammad Hakimi <52748583+mohammd-hakimi@users.noreply.github.com>
2020-10-07 02:11:07 +0330

    Updates

---
+++ precache-manifest.ff488f5ef6b94caba541b639df704413.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbe5428d16e541cd8973882ca2774345",
+    "url": "/first-app/index.html"
+  },
+  {
+    "revision": "34f60f8cbe116a72c64a",
+    "url": "/first-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a76944f2fda085ce176b",
+    "url": "/first-app/static/js/2.470c3ac7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/first-app/static/js/2.470c3ac7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34f60f8cbe116a72c64a",
+    "url": "/first-app/static/js/main.0ccef7c7.chunk.js"
+  },
+  {
+    "revision": "0411795b630819b8d466",
+    "url": "/first-app/static/js/runtime-main.284ef3e7.js"
+  },
+  {
+    "revision": "4e80f86919b04422d853db8acd99a063",
+    "url": "/first-app/static/media/Bouncy-Preloader.4e80f869.gif"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/first-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
